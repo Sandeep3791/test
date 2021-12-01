d4f1c377e1b257a4cd1cdb8b37f015e10137ec36
pankaj.s <pankaj.s@jointworksolutions.com>
2021-12-01 19:34:52 +0530

    Bug fixing

--- wayrem_admin/forms/roles.py
+++ wayrem_admin/forms/roles.py
@@ -1,10 +1,16 @@
 from django import forms
+from django.core.validators import validate_image_file_extension
 from wayrem_admin.models import Roles
 
 
 class RoleForm(forms.ModelForm):
     content = forms.CharField(widget=forms.Textarea(
         attrs={'class': 'form-control', 'rows': '3', 'placeholder': 'Add Role Description...'}))
+    
+    choice_status = (("Active", "Active"), ("Inactive", "Inactive"))
+
+    status = forms.ChoiceField(
+        choices=choice_status, widget=forms.Select(attrs={'class': 'form-select',}))
 
     class Meta:
         model = Roles
@@ -14,9 +20,9 @@ class RoleForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         for visible in self.visible_fields():
             if visible.field.required:
-                visible.field.widget.attrs['class'] = 'form-control required-cls'
+                visible.field.widget.attrs['class'] = 'form-control  required-cls'
             else:
-                visible.field.widget.attrs['class'] = 'form-control'
+                visible.field.widget.attrs['class'] = 'form-control form-select'
 
 
 class RoleViewForm(forms.ModelForm):
