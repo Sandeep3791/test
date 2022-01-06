e61af7a19f71ed691391fd9c3d937b0a3fcefd8d
Suryaaa <rathoresuryaaa21@gmail.com>
2022-01-07 02:23:24 +0530

    media testing server

--- wayrem_admin/models.py
+++ wayrem_admin/models.py
@@ -109,7 +109,7 @@ class Categories(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
     name = models.CharField(max_length=35, unique=True)
     image = models.ImageField(
-        upload_to='assets/category/', storage=upload_storage, default='assets/category/category.jpg', blank=False, null=True)
+        upload_to='', storage=upload_storage, default='category.jpg', blank=False, null=True)
     tag = models.TextField(null=True, blank=True)
     parent = models.CharField(max_length=35,  null=True)
     margin = models.IntegerField()
@@ -132,7 +132,7 @@ class SubCategories(models.Model):
     tag = models.TextField(null=True, blank=True)
     margin = models.IntegerField()
     image = models.ImageField(
-        upload_to='assets/subcategory/', storage=upload_storage, blank=False, null=True)
+        upload_to='', storage=upload_storage, blank=False, null=True)
     category = models.ForeignKey(
         Categories, on_delete=models.CASCADE, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -166,7 +166,7 @@ class Supplier(models.Model):
     password = models.CharField(max_length=200)
     contact = models.BigIntegerField(null=True)
     logo = models.ImageField(
-        upload_to='supplier/', storage=upload_storage,  null=True, default='supplier/default.jpg')
+        upload_to='', storage=upload_storage,  null=True, default='default.jpg')
     address = models.TextField(null=True, blank=True)
     delivery_incharge = models.CharField(max_length=500, blank=True, null=True)
     company_name = models.CharField(max_length=100, blank=False, null=True)
@@ -247,7 +247,7 @@ class Products(models.Model):
     margin_unit = models.CharField(
         max_length=20, choices=DIS_ABS_PERCENT, null=True, blank=False)
     primary_image = models.ImageField(
-        upload_to='product/images/', storage=upload_storage, default='product/images/product.jpg', null=True)
+        upload_to='', storage=upload_storage, default='product.jpg', null=True)
     gs1 = models.CharField(max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -274,7 +274,7 @@ class Images(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
     product = models.ForeignKey(
         Products, on_delete=models.CASCADE, default=None)
-    image = models.FileField(upload_to="product/images/", storage=upload_storage,
+    image = models.FileField(upload_to="", storage=upload_storage,
                              verbose_name='product_mage')
 
     class Meta:
@@ -395,7 +395,7 @@ class Invoice(models.Model):
     invoice_id = models.AutoField(primary_key=True, unique=True)
     invoice_no = models.CharField(max_length=250, null=True)
     po_name = models.CharField(max_length=250, null=True)
-    file = models.FileField(upload_to='invoices/', storage=upload_storage,
+    file = models.FileField(upload_to='', storage=upload_storage,
                             null=True)
     supplier_name = models.CharField(max_length=250, null=True)
     invoice_status = (
