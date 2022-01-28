bd60299626eda8602cf62f4f005d690d95e70c03
Suryaaa <rathoresuryaaa21@gmail.com>
2022-01-28 13:03:06 +0530

    testing

--- wayrem/settings.py
+++ wayrem/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'wayrem.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'wayrem_test_v1',
+        'NAME': 'wayrem_test_v2',
         'USER': 'wayremdb_adminuser',
         'PASSWORD': 'r2wXJfUs78NczMFWRXcV',
         'HOST': '15.185.103.226',
