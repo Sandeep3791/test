5a7db2361e2883d739ae40223021943babd8bfed
Suryaaa <rathoresuryaaa21@gmail.com>
2022-02-07 14:08:04 +0530

    redis settings

--- wayrem/settings.py
+++ wayrem/settings.py
@@ -200,9 +200,11 @@ CKEDITOR_CONFIGS = {
 
 
 # celery
+CELERY_BROKER_URL = 'redis://redis-server:6379'
+CELERY_RESULT_BACKEND = 'redis://redis-server:6379'
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379'
+# CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
+# CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
