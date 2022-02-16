c9a4eda1db04aff30bc26d915e16ddcc44547e6e
Suryaaa <rathoresuryaaa21@gmail.com>
2022-02-16 21:54:47 +0530

    database changed

--- wayrem_admin/models_recurrence.py
+++ wayrem_admin/models_recurrence.py
@@ -1,6 +1,7 @@
 from django.db import models
 from .models import *
 
+
 class RecurrentType(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True)
     value = models.SmallIntegerField(blank=True, null=True)
@@ -10,10 +11,13 @@ class RecurrentType(models.Model):
         app_label = "wayrem_admin"
         db_table = 'recurrent_type'
 
+
 class RecurrenceGrocery(models.Model):
     customer_id = models.IntegerField(blank=True, null=True)
-    grocery = models.ForeignKey('GroceryMaster', models.DO_NOTHING, blank=True, null=True)
-    recurrenttype = models.ForeignKey('RecurrentType', models.DO_NOTHING, db_column='recurrenttype', blank=True, null=True)
+    grocery = models.ForeignKey(
+        'GroceryMaster', models.DO_NOTHING, blank=True, null=True)
+    recurrenttype = models.ForeignKey(
+        'RecurrentType', models.DO_NOTHING, db_column='recurrenttype', blank=True, null=True)
     recurrence_startdate = models.CharField(max_length=255)
     recurrence_nextdate = models.CharField(max_length=255)
     status = models.IntegerField(blank=True, null=True)
@@ -28,8 +32,10 @@ class RecurrenceGrocery(models.Model):
 class GroceryMaster(models.Model):
     grocery_name = models.CharField(max_length=255, blank=True, null=True)
     description = models.CharField(max_length=1000, blank=True, null=True)
-    customer = models.ForeignKey('wayrem_admin.Customer', models.DO_NOTHING, blank=True, null=True)
-    address = models.ForeignKey('wayrem_admin.CustomerAddresses', models.DO_NOTHING, blank=True, null=True)
+    customer = models.ForeignKey(
+        'wayrem_admin.Customer', models.DO_NOTHING, blank=True, null=True)
+    address = models.ForeignKey(
+        'wayrem_admin.CustomerAddresses', models.DO_NOTHING, blank=True, null=True)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
@@ -37,15 +43,19 @@ class GroceryMaster(models.Model):
         app_label = "wayrem_admin"
         db_table = 'grocery_master'
 
+
 class GroceryProducts(models.Model):
-    grocery = models.ForeignKey('GroceryMaster', models.DO_NOTHING, blank=True, null=True)
+    grocery = models.ForeignKey(
+        'GroceryMaster', models.DO_NOTHING, blank=True, null=True)
     product = models.ForeignKey('wayrem_admin.Products', models.DO_NOTHING)
     product_qty = models.IntegerField(blank=True, null=True)
-    recurrence_nextdate = models.CharField(max_length=255, blank=True, null=True)
+    recurrence_nextdate = models.CharField(
+        max_length=255, blank=True, null=True)
 
     class Meta:
         app_label = "wayrem_admin"
         db_table = 'grocery_products'
+
 
 class ForecastJobtype(models.Model):
     id = models.SmallAutoField(primary_key=True)
@@ -56,6 +66,7 @@ class ForecastJobtype(models.Model):
         app_label = "wayrem_admin"
         db_table = 'forecast_jobtype'
 
+
 class ForecastProduct(models.Model):
     id = models.BigAutoField(primary_key=True)
     forecast_jobtype_id = models.SmallIntegerField()
@@ -64,7 +75,6 @@ class ForecastProduct(models.Model):
     forecast_quantity = models.SmallIntegerField()
     needed_stock_purchase = models.SmallIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
-    
 
     class Meta:
         app_label = "wayrem_admin"
