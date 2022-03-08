b998d381c94b412ad3ef1b46cfb2056ecc4816ba
Suryaaa <rathoresuryaaa21@gmail.com>
2022-03-08 11:44:01 +0530

    po forecast fix

--- wayrem_admin/templatetags/util_tags.py
+++ wayrem_admin/templatetags/util_tags.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
+from wayrem_admin.models import Products
+from wayrem_admin.services import inst_SupplierProduct
 from django import template
 from django.template import Context
 from datetime import date, datetime
@@ -11,15 +13,19 @@ register = template.Library()
 
 @register.filter(name='display_discount_price')
 def display_discount_price(order_detail_id):
-    order_details=OrderDetails.objects.filter(id=order_detail_id).first()
-    total_amount=float(order_details.price) + float(order_details.item_margin) -  float(order_details.discount)
-    return round(total_amount,2)
+    order_details = OrderDetails.objects.filter(id=order_detail_id).first()
+    total_amount = float(order_details.price) + \
+        float(order_details.item_margin) - float(order_details.discount)
+    return round(total_amount, 2)
+
 
 @register.filter(name='display_discount_price_qty')
 def display_discount_price_qty(order_detail_id):
-    order_details=OrderDetails.objects.filter(id=order_detail_id).first()
-    total_amount=(float(order_details.price) + float(order_details.item_margin) - float(order_details.discount)) * float(order_details.quantity) 
-    return round(total_amount,2)
+    order_details = OrderDetails.objects.filter(id=order_detail_id).first()
+    total_amount = (float(order_details.price) + float(order_details.item_margin) -
+                    float(order_details.discount)) * float(order_details.quantity)
+    return round(total_amount, 2)
+
 
 @register.filter(name='total_items')
 def total_items(order_id):
@@ -51,6 +57,7 @@ def payment_status(order_id):
     else:
         return ""
 
+
 @register.filter(name='payment_type')
 def payment_type(order_id):
     # return order_id
@@ -60,6 +67,7 @@ def payment_type(order_id):
         return order_transaction.payment_mode
     else:
         return ""
+
 
 @register.filter(name='transaction_invoice')
 def transaction_invoice(order_id):
@@ -79,9 +87,21 @@ def unit_product_total(ord_detail_id):
     total_amount = ((float(ord_det_id.price) + float(ord_det_id.item_margin))
                     * float(ord_det_id.quantity))-float(ord_det_id.discount)
     return total_amount
+
+
 @register.filter(name='forecast_quantity')
-def forecast_quantity(no_of_day,product_id):
-    get_forecast_quantity=ForecastProduct.objects.values("forecast_quantity").filter(forecast_jobtype_id=no_of_day,product_id=product_id,created_at__gte=date.today()).order_by("-id").first()
+def forecast_quantity(no_of_day, product_id):
+    # print(no_of_day)
+    # print(product_id)
+    x = inst_SupplierProduct(product_id)
+    print(x.SKU)
+    product_SKU = x.SKU
+    product_id = Products.objects.get(SKU=product_SKU)
+    get_forecast_quantity = ForecastProduct.objects.values("forecast_quantity").filter(
+        forecast_jobtype_id=no_of_day, product_id=product_id.id, created_at__gte=date.today()).order_by("-id").first()
+    print(product_id.id)
+    print(ForecastProduct.objects.values("forecast_quantity").filter(
+        forecast_jobtype_id=no_of_day, product_id=product_id.id, created_at__gte=date.today()).order_by("-id").query)
     if get_forecast_quantity is None:
         return 0
     else:
