265d0241dd0d6005e0a21d71f2209a5bfd43160e
Suryaaa <rathoresuryaaa21@gmail.com>
2022-03-23 12:48:38 +0530

    cronjob modify

--- cron_job_migration.sh
+++ cron_job_migration.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "hello world"
+echo "Migrations Initiated..."
+sleep 2
 FILE=/opt/app/wayrem-admin-backend/newmigration
 # FILE=/home/fealty/Documents/Dynamic_Model/admin/wayrem-admin-backend/newmigration
 echo "$FILE"
@@ -7,8 +8,10 @@ if test -f "$FILE"; then
     echo "$FILE exists."
     # /bin/python /opt/app/wayrem-admin-backend/manage.py makemigrations
     /usr/local/bin/python3.8 /opt/app/wayrem-admin-backend/manage.py makemigrations wayrem_admin
-    sleep 2
+    sleep 3
     /usr/local/bin/python3.8 /opt/app/wayrem-admin-backend/manage.py migrate
+
+    echo "Migrations Completed..."
     # /usr/bin/python3.8 /home/fealty/Documents/Dynamic_Model/admin/wayrem-admin-backend/manage.py makemigrations
     # sleep 3
     # /usr/bin/python3.8 /home/fealty/Documents/Dynamic_Model/admin/wayrem-admin-backend/manage.py migrate
