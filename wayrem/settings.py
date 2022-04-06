8665ad54bdf149750719f639ffc6e0536173def3
Suryaaa <rathoresuryaaa21@gmail.com>
2022-04-06 18:24:40 +0530

    required in product import removed

--- wayrem/settings.py
+++ wayrem/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'wayrem.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'wayrem_testing',
+        'NAME': 'wayrem_stg_v1',
         'USER': 'wayremdb_adminuser',
         'PASSWORD': 'r2wXJfUs78NczMFWRXcV',
         'HOST': '15.185.103.226',
@@ -139,8 +139,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'wayrem_admin.User'
 MESSAGE_TAGS = {messages_s.ERROR: 'danger'}
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_ROOT = os.path.join('/home/fealty/Desktop/wayrem_kapil/backup/')
-# MEDIA_ROOT = os.path.join('common_folder')
 MEDIA_ROOT = os.path.dirname(
     "/opt/app/wayrem-admin-backend/media/common_folder/")
 MEDIA_URL = '/media/common_folder/'
