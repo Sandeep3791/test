cb7f801912a5c0d0a0b9032e29a2a5cd1266598f
Suryaaa <rathoresuryaaa21@gmail.com>
2022-05-23 11:48:12 +0530

    email change to smtp

--- wayrem_admin/services.py
+++ wayrem_admin/services.py
@@ -4,49 +4,37 @@ from wayrem_admin.models import *
 import requests
 import json
 from mailersend import emails
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 def send_email(to, subject, body):
-    api_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNTIwY2IyZGRjMzI1ZDdjM2FjYjBlNjdlMDkxNDA2MjU1ZmQzNjAwNGNiZDIxZDdhZDE1YTc2MjkyZjhmNmZiZmY1MDNlOTg1NDllNjEyYWQiLCJpYXQiOjE2NDYzODg1MDcuNjU2MDM4LCJuYmYiOjE2NDYzODg1MDcuNjU2MDQxLCJleHAiOjQ4MDIwNjIxMDcuNjQ3Mzk1LCJzdWIiOiIyMjI3NyIsInNjb3BlcyI6WyJlbWFpbF9mdWxsIiwiZG9tYWluc19mdWxsIiwiYWN0aXZpdHlfZnVsbCIsImFuYWx5dGljc19mdWxsIiwidG9rZW5zX2Z1bGwiLCJ3ZWJob29rc19mdWxsIiwidGVtcGxhdGVzX2Z1bGwiLCJzdXBwcmVzc2lvbnNfZnVsbCJdfQ.n2M_k8A5451zVfTULF-xj_AQP6hE10rEG5NrBpk-iG_rySVTmQRYRtjPJUdkhrl1DzG3Ykt61y_BVTIoYVlUiza0F0HsBxrjzvYqHlBWV73lBUj1MyAeKf2jkhYpPgSHq7ZO6cLO0jVKw1oKy9Fr4Ijh72o15qJjjILIkcPF2yCfz_jnO7q21p9BokZfM_y1wAFHivXUKrjJt59TMt_I4A8zw3RiKbcd6gz8UItO1hRvaQ00zNTid-zL17stqPE63ieEOz14cLpN_IHCIQXZ_xrFOOp59xR7tHil1JQYlZ76__QJw4wj4MGhA7n0ap0GXQk98E4h14tiDZcf0PeNRD6nI_uC6nZK-S1ZziNaAWMF9fi-P1GwFMn-lXdf510RawzWsOI_F3SYVLRfN5B2iiID-QmEzXD-r7BuRNWAoGVCUP_h5ONLcP5qzpL8zQOH7nd6varQ3Luduphz8HdjaGoNRXJGoUjYJQ96X9ejlRcs8PJsiRx6O2Dtr6fiHozn5NSk_wM-f9DFFJxgzp51wBiL0fWaVJYFKa0c0dtY0087_RjRtjAzXrlE-uuUlEA0TzjCl12qZRAUOSZxABkKeBDRPDmFk44ElKGMgObD47HNwEq8RI2sKiubtUjP9RuVybaHvvfarqdSylqkvAjjVhgS6JK2YEbF3dM2Mrt-rcg"
 
-    mailer = emails.NewEmail(api_key)
-    # define an empty dict to populate with mail values
-    mail_body = {}
-
-    mail_from = {
-        "name": "Wayrem",
-        "email": "wayrem@admin-uat.wayrem.com",
-    }
-
-    recipients = [
-        {
-            "name": None,
-            "email": to,
-        }
-    ]
-
-    reply_to = [
-        {
-            "name": "Wayrem",
-            "email": "wayrem@admin-uat.wayrem.com",
-        }
-    ]
+    sender = 'support@wayrem.com'
+    password = 'Wayrem@123'
+    receiver = to
+    smtpserver = smtplib.SMTP("smtp-mail.outlook.com", 587)
     try:
-        mailer.set_mail_from(mail_from, mail_body)
-        mailer.set_mail_to(recipients, mail_body)
-        mailer.set_subject(subject, mail_body)
-        mailer.set_html_content(body, mail_body)
-        # mailer.set_plaintext_content("This is the text content", mail_body)
-        # mailer.set_reply_to(reply_to, mail_body)
-
-        # using print() will also return status code and data
-        print(mailer.send(mail_body))
-        print("Email sent Successfully!!")
+        FROM = 'support@wayrem.com'
+        TO = to
+        REPLY_TO_ADDRESS = 'no-reply@wayrem.com'
+        msg = MIMEMultipart('alternative')
+        msg['To'] = TO
+        msg['From'] = FROM
+        msg['Subject'] = subject
+        msg.add_header('reply-to', REPLY_TO_ADDRESS)
+        mt_html = MIMEText(body, 'html')
+        msg.attach(mt_html)
+        mail = smtplib.SMTP('smtp-mail.outlook.com', 587)
+        mail.ehlo()
+        mail.starttls()
+        mail.login('support@wayrem.com', 'Wayrem@123')
+        mail.sendmail(FROM, TO, msg.as_string())
+        print("Email Sent!")
+        mail.quit()
     except Exception as e:
-        print("Email not sent!!")
         print(e)
-        print(e.body)
-
 
 # def send_email(to, subject, body):
 #     message = Mail(
@@ -132,3 +120,44 @@ def inst_Unit(value):
 
 def inst_Warehouse(value):
     return Warehouse.objects.get(id=value)
+
+# def send_email(to, subject, body):
+#     api_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNTIwY2IyZGRjMzI1ZDdjM2FjYjBlNjdlMDkxNDA2MjU1ZmQzNjAwNGNiZDIxZDdhZDE1YTc2MjkyZjhmNmZiZmY1MDNlOTg1NDllNjEyYWQiLCJpYXQiOjE2NDYzODg1MDcuNjU2MDM4LCJuYmYiOjE2NDYzODg1MDcuNjU2MDQxLCJleHAiOjQ4MDIwNjIxMDcuNjQ3Mzk1LCJzdWIiOiIyMjI3NyIsInNjb3BlcyI6WyJlbWFpbF9mdWxsIiwiZG9tYWluc19mdWxsIiwiYWN0aXZpdHlfZnVsbCIsImFuYWx5dGljc19mdWxsIiwidG9rZW5zX2Z1bGwiLCJ3ZWJob29rc19mdWxsIiwidGVtcGxhdGVzX2Z1bGwiLCJzdXBwcmVzc2lvbnNfZnVsbCJdfQ.n2M_k8A5451zVfTULF-xj_AQP6hE10rEG5NrBpk-iG_rySVTmQRYRtjPJUdkhrl1DzG3Ykt61y_BVTIoYVlUiza0F0HsBxrjzvYqHlBWV73lBUj1MyAeKf2jkhYpPgSHq7ZO6cLO0jVKw1oKy9Fr4Ijh72o15qJjjILIkcPF2yCfz_jnO7q21p9BokZfM_y1wAFHivXUKrjJt59TMt_I4A8zw3RiKbcd6gz8UItO1hRvaQ00zNTid-zL17stqPE63ieEOz14cLpN_IHCIQXZ_xrFOOp59xR7tHil1JQYlZ76__QJw4wj4MGhA7n0ap0GXQk98E4h14tiDZcf0PeNRD6nI_uC6nZK-S1ZziNaAWMF9fi-P1GwFMn-lXdf510RawzWsOI_F3SYVLRfN5B2iiID-QmEzXD-r7BuRNWAoGVCUP_h5ONLcP5qzpL8zQOH7nd6varQ3Luduphz8HdjaGoNRXJGoUjYJQ96X9ejlRcs8PJsiRx6O2Dtr6fiHozn5NSk_wM-f9DFFJxgzp51wBiL0fWaVJYFKa0c0dtY0087_RjRtjAzXrlE-uuUlEA0TzjCl12qZRAUOSZxABkKeBDRPDmFk44ElKGMgObD47HNwEq8RI2sKiubtUjP9RuVybaHvvfarqdSylqkvAjjVhgS6JK2YEbF3dM2Mrt-rcg"
+
+#     mailer = emails.NewEmail(api_key)
+#     # define an empty dict to populate with mail values
+#     mail_body = {}
+
+#     mail_from = {
+#         "name": "Wayrem",
+#         "email": "wayrem@admin-uat.wayrem.com",
+#     }
+
+#     recipients = [
+#         {
+#             "name": None,
+#             "email": to,
+#         }
+#     ]
+
+#     reply_to = [
+#         {
+#             "name": "Wayrem",
+#             "email": "wayrem@admin-uat.wayrem.com",
+#         }
+#     ]
+#     try:
+#         mailer.set_mail_from(mail_from, mail_body)
+#         mailer.set_mail_to(recipients, mail_body)
+#         mailer.set_subject(subject, mail_body)
+#         mailer.set_html_content(body, mail_body)
+#         # mailer.set_plaintext_content("This is the text content", mail_body)
+#         # mailer.set_reply_to(reply_to, mail_body)
+
+#         # using print() will also return status code and data
+#         print(mailer.send(mail_body))
+#         print("Email sent Successfully!!")
+#     except Exception as e:
+#         print("Email not sent!!")
+#         print(e)
+#         print(e.body)
