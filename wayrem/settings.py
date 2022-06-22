59f3af702298dd53b0bbbbde3ef120327bfb9187
Suryaaa Rathore <surya.r@jointworksolutions.com>
2022-06-22 17:10:33 +0530

    stg setup with role module

--- wayrem/settings.py
+++ wayrem/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'wayrem_stg_v1.2',
         'USER': 'wayremdb_adminuser',
         'PASSWORD': 'r2wXJfUs78NczMFWRXcV',
-        'HOST': '157.175.151.40',
+        'HOST': 'db',
         'PORT': '3306',
         'OPTIONS': {
             "init_command": "SET foreign_key_checks = 0;",
@@ -130,7 +130,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kuwait'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
