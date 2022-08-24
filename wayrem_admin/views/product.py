92b33d92b9a02fec9ae4f973116655fcf8471a9f
Suryaaa Rathore <surya.r@jointworksolutions.com>
2022-08-24 18:28:22 +0530

    primary image function

--- wayrem_admin/views/product.py
+++ wayrem_admin/views/product.py
@@ -1035,3 +1035,52 @@ def scan_result(request):
     except Exception as e:
         print(e)
         return render(request, "product/product_view_pop.html", {"message": "Barcode is not available!!"})
+
+
+def import_primary_image(request):
+    # path = '/home/suryaaa/Music/image_testing/client-images'
+    path = os.path.join(os.path.abspath(
+        '.'), "media", "common_folder", "import_images")
+    # path = '/opt/app/wayrem-admin-backend/media/wayrem-product-images'
+
+    items = [f for f in os.listdir(
+        path) if not os.path.isdir(os.path.join(path, f))]
+
+    print(items)
+
+    for file in items:
+        file_name = file.split(".")[0].lower()
+        product = Products.objects.filter(name__icontains=file_name).first()
+        # product = Products.objects.filter(SKU=i).first()
+        common_folder = os.path.join(
+            os.path.abspath('.'), "media", "common_folder")
+        if product:
+            src_dir = os.path.join(path, file)
+            # dst_dir = f"/home/suryaaa/Music/database/products/{i}/"
+            # dst_dir = f"{common_folder}/products/{product.SKU}/"
+            dst_dir = os.path.join(common_folder, "products", product.SKU)
+            isExist = os.path.exists(dst_dir)
+            if not isExist:
+                os.makedirs(dst_dir)
+            if os.path.isfile(src_dir):
+                destination = os.path.join(dst_dir, file.replace(' ', '_'))
+                shutil.copy(src_dir, destination)
+                product.primary_image = f"products/{product.SKU}/{file.replace(' ', '_')}"
+                print("default image copied")
+                product.save()
+                print('copied', file_name)
+            os.remove(src_dir)
+        else:
+            source = os.path.join(path, file)
+            # dst_dir = f"/home/suryaaa/Music/image_testing/failed"
+            dst_dir = os.path.join(common_folder, "failed")
+            isExist = os.path.exists(dst_dir)
+            if not isExist:
+                os.makedirs(dst_dir)
+            destination = os.path.join(dst_dir, file)
+            shutil.copy(source, destination)
+            print('copied', file)
+            os.remove(source)
+            print("failed!!")
+    print("done")
+    return HttpResponse("Successfully imported!!")
