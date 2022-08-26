70a7dd4e42d2251d3a4ef3777594078f5592a8c0
Kapil <kapil.g@jointworksolutions.com>
2022-08-26 14:58:36 +0530

    clone order

--- wayrem_admin/views/wallet.py
+++ wayrem_admin/views/wallet.py
@@ -20,6 +20,41 @@ from django.views.decorators.csrf import csrf_exempt
 from wayrem_admin.permissions.mixins import LoginPermissionCheckMixin
 from django.db.models import Sum
 
+class WalletCustomerPay(View):
+    def get(self,request,id):
+        amount=self.wallet_total_amount(id)
+        self.debit_to_wallet(id,amount)
+        
+        return HttpResponseRedirect("/wallet/"+str(id))
+
+    def wallet_total_amount(self,customer_id):
+        transaction_id=Wallet.objects.values('transaction_type_id').annotate(total_sum=Sum('amount')).filter(customer_id=customer_id)
+        dr=0
+        cr=0
+        for tr in transaction_id:
+            if tr['transaction_type_id'] == 1:
+                dr=tr['total_sum']
+            else:
+                cr=tr['total_sum']
+        total_amount=cr - dr
+        if total_amount <= 0:
+            return 0
+        else:
+            return total_amount
+    
+    def debit_to_wallet(self,customer_id,amount):
+        from datetime import datetime
+        grand_total=amount
+        currentDateTime = datetime.now()
+        payment_type=30
+        orderObject=Wallet.objects.filter(transaction_type_id=2).order_by("-id").first()
+        order_id=orderObject.order_id
+
+        total_amount=float(grand_total)
+        wallet={'amount':total_amount,'payment_type_id':payment_type,'transaction_type_id':1,'order_id':order_id,'customer_id':customer_id,'created':currentDateTime}
+        wallet=Wallet(**wallet)
+        wallet.save()
+        return 1
 
 class WalletList(ListView):
     login_url = 'wayrem_admin:root'
@@ -50,6 +85,7 @@ class WalletList(ListView):
         if total_amount <= 0:
             total_amount=0
         context['total_amount']=total_amount
+        context['customer_id']=id
         return context
 
 
