abcf080faf6ea54e597d759bf64ee02f69a2f3e9
Suryaaa Rathore <surya.r@jointworksolutions.com>
2022-09-01 17:19:55 +0530

    stg setup

--- wayrem/constant.py
+++ wayrem/constant.py
@@ -1,8 +1,8 @@
 # # For Server
 from django.core.files.storage import FileSystemStorage
-WAYREM_ADMIN_BASE_URL = "https://admin-uat.wayrem.com/"
-WAYREM_SUPPLIER_BASE_URL = "https://supplier-uat.wayrem.com/"
-DOCUMENT_URL = "https://admin-uat.wayrem.com/media/common_folder/"
+WAYREM_ADMIN_BASE_URL = "https://admin-stg.wayrem.com/"
+WAYREM_SUPPLIER_BASE_URL = "https://supplier-stg.wayrem.com/"
+DOCUMENT_URL = "https://admin-stg.wayrem.com/media/common_folder/"
 
 # For Local
 # WAYREM_ADMIN_BASE_URL = "http://127.0.0.1:8000/"
